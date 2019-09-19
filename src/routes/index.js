a8b8eb0208dddf71dd35c582fbe01259dfbf1679
HusseinOsman <hussein.mostafa.osman@gmail.com>
2019-09-19 13:17:21 +0200

    add jugglingdb as ORM with mongo configration

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,32 @@
 // routes/index.js
 import express from 'express';
+import User from '../app/models/user';
+
 var router = express.Router();
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
-    title: 'Express'
+    title: 'Arabica.js'
   });
 });
-router.get('/test', function (req, res, next) {
-  res.json({
-    title: 'test'
+
+router.post('/register', (req, res) => {
+  var post = new User();
+  post.name = "Arabica";
+  post.password = "arabica";
+  post.save((err, data) => {
+    console.log(err, data);
+    res.status(200).send({
+      data: data
+    });
   });
 });
+
+router.post('/login', (req, res) => {
+  res.status(200).send({
+    access_token: ''
+  });
+});
+
 export default router;
