fb5818a3d48f4ed477b4b168efc29ae9537441e5
HusseinOsman <hussein.mostafa.osman@gmail.com>
2019-11-17 12:58:06 +0200

    enable CORS

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import indexRouter from './routes/index';
+import cors from 'cors';
+
 const app = express();
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
