26a80a92e7da09e401e7b760144235e4cea2a6f8
HusseinOsman <hussein.mostafa.osman@gmail.com>
2022-01-15 19:23:53 +0200

    update response helper for no content response 204

--- src/app/helper/response.js
+++ src/app/helper/response.js
@@ -1,11 +1,11 @@
 class Response {
 
     static error(res, message, code, status) {
-        console.log("return err ===============================",  message);
+        console.log("return err ===============================", message);
         status = typeof status !== 'undefined' ? status : 400;
         if (typeof message === 'string') {
             const tmpMessage = message;
-            message=[];
+            message = [];
             message.push({
                 path: 'err',
                 message: tmpMessage
@@ -23,31 +23,27 @@ class Response {
         res.type('application/json');
         res.status(status).send(data);
     };
-
-    static data(res, data, key, status) {
-        status = typeof status !== 'undefined' ? status : 200;
-        key = typeof key !== 'undefined' ? key : 'data';
-        res.type('application/json');
-        const newData = {
-            'success': true
-        };
-        newData[key] = data;
-        res.status(status).send(newData);
-    };
-
+    
     static rawData(res, data, status) {
         status = typeof status !== 'undefined' ? status : 200;
         res.type('application/json');
+        if (!this._checkContent(data))
+            return res.status(204).send();
         res.status(status).send(data);
     };
 
-    static pagingData(res, data, key, offset, limit, total, status) {
+    static pagingData(res, data, offset, limit, total, key, status) {
         status = typeof status !== 'undefined' ? status : 200;
         key = typeof key !== 'undefined' ? key : 'data';
         res.type('application/json');
+
+        if (!this._checkContent(data))
+            return res.status(204).send();
+
         const newData = {
             'success': true
         };
+
         newData[key] = data;
         newData.offset = offset;
         newData.limit = limit;
@@ -63,6 +59,13 @@ class Response {
         res.type('application/json');
         res.status(status).send(data);
     };
+
+    static _checkContent(data) {
+        if (data === undefined)
+            return false;
+        return (data.length == 0 || Object.keys(data).length === 0) ? false : true;
+
+    }
 }
 
 export default Response;
